3ac344f9bba9ebae170be43e87e287bd7b040b5b
hao1635 <61267697+hao1635@users.noreply.github.com>
2024-03-24 16:07:19 +0800

    Delete LEDA directory

--- LEDA/main.sh
+++
@@ -1,6 +0,0 @@
-##train
-python train.py  --name LEDA   --model LEDA  --netG  redcnn  --dataroot /data/zhchen/Mayo2016_2d --lr 0.0001 --gpu_ids 7 --print_freq 25 --batch_size 1 --lr_policy cosine
-
-
-##test
-python test.py  --name LEDA   --model LEDA  --netG redcnn  --result_name LEDA_results   --gpu_ids 6 --batch_size 1 --eval
